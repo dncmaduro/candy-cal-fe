52a2b17ee144ee572f50300718adcc614c2803ec
dncmaduro <manhdungtran.vnuuet@gmail.com>
2025-08-12 17:48:30 +0700

    fix: access control

--- src/constants/navs.ts
+++ src/constants/navs.ts
@@ -5,26 +5,26 @@ export const NAVS = [
   {
     to: `${NAVS_URL}/storage`,
     label: "Kho chứa",
-    roles: ["order-emp"],
+    roles: ["order-emp", "system-emp"],
     icon: "IconBox"
   },
   {
     to: `${NAVS_URL}/calfile`,
     label: "Tính đơn vận",
-    roles: ["order-emp", "accounting-emp"],
+    roles: ["order-emp", "accounting-emp", "system-emp"],
     icon: "IconCalculator"
   },
   {
     to: `${NAVS_URL}/orders-logs`,
     label: "Lịch sử vận đơn",
-    roles: ["order-emp"],
+    roles: ["order-emp", "system-emp"],
     icon: "IconHistory",
     deprecated: true
   },
   {
     to: `${NAVS_URL}/logs`,
     label: "Lịch sử kho",
-    roles: ["admin", "order-emp", "accounting-emp"],
+    roles: ["admin", "order-emp", "accounting-emp", "system-emp"],
     icon: "IconHistory",
     beta: true
   },
@@ -32,26 +32,26 @@ export const NAVS = [
     to: `${NAVS_URL}/accounting-storage`,
     label: "Kho hàng",
     icon: "IconBox",
-    roles: ["accounting-emp"]
+    roles: ["accounting-emp", "system-emp"]
   },
   {
     to: `${NAVS_URL}/delivered-requests`,
     label: "Yêu cầu xuất hàng",
     icon: "IconTruck",
-    roles: ["accounting-emp", "order-emp"]
+    roles: ["accounting-emp", "order-emp", "system-emp"]
   },
   {
     to: `${NAVS_URL}/incomes`,
     label: "Doanh thu",
     icon: "IconCoin",
-    roles: ["accounting-emp", "order-emp"],
+    roles: ["accounting-emp", "order-emp", "system-emp"],
     beta: true
   },
   {
     to: `${NAVS_URL}/system-logs`,
     label: "Lịch sử hệ thống",
     icon: "IconAutomaticGearbox",
-    roles: ["admin"],
+    roles: ["admin", "system-emp"],
     beta: true
   }
 ]
@@ -60,6 +60,6 @@ export const LANDING_NAVS = [
   {
     to: `${LANDING_NAVS_URL}/landing-page`,
     label: "Trang chủ",
-    roles: ["order-emp"]
+    roles: ["order-emp", "system-emp"]
   }
 ]
