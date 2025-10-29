a247260e9bf0522ce6072305033ba49088760ee8
dungtm-3608 <tran.manh.dung@sun-asterisk.com>
2025-10-29 17:27:44 +0700

    feat: save pre day ads cost

--- src/hooks/useDailyAds.ts
+++ src/hooks/useDailyAds.ts
@@ -1,6 +1,11 @@
 import { useUserStore } from "../store/userStore"
+import { toQueryString } from "../utils/toQuery"
 import { callApi } from "./axios"
-import { CreateDailyAdsRequest } from "./models"
+import {
+  CreateDailyAdsRequest,
+  GetPreviousDailyAdsBefore4pmRequest,
+  GetPreviousDailyAdsBefore4pmResponse
+} from "./models"
 
 export const useDailyAds = () => {
   const { accessToken } = useUserStore()
@@ -30,5 +35,46 @@ export const useDailyAds = () => {
     })
   }
 
-  return { createDailyAds }
+  const createDailyAdsWithSavedAdsCost = async (
+    files: File[],
+    req: CreateDailyAdsRequest
+  ) => {
+    const formData = new FormData()
+    formData.append("files", files[0])
+    formData.append("files", files[1])
+    formData.append("files", files[2])
+    formData.append("files", files[3])
+
+    Object.entries(req).forEach(([key, value]) => {
+      if (typeof value !== "undefined" && value !== null)
+        formData.append(key, value as string)
+    })
+
+    return callApi<FormData, never>({
+      path: `/v1/dailyads/update-with-saved-before4pm`,
+      data: formData,
+      method: "POST",
+      token: accessToken,
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    })
+  }
+
+  const getPreviousDailyAds = async (
+    req: GetPreviousDailyAdsBefore4pmRequest
+  ) => {
+    const previousDate = new Date(req.date)
+    previousDate.setDate(previousDate.getDate() - 1)
+    req = { ...req, date: previousDate }
+    const query = toQueryString(req)
+
+    return callApi<never, GetPreviousDailyAdsBefore4pmResponse>({
+      path: `/v1/dailyads/before4pm?${query}`,
+      method: "GET",
+      token: accessToken
+    })
+  }
+
+  return { createDailyAds, createDailyAdsWithSavedAdsCost, getPreviousDailyAds }
 }
