a7f4692e7c5c3a106532c6037ef86af6a0ac6ba2
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2026-01-03 15:38:14 +0700

    feat: pwa (#209)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
+import { registerSW } from "virtual:pwa-register"
+
+registerSW({
+  onNeedRefresh() {
+    console.log("Need refresh")
+  },
+  onOfflineReady() {
+    console.log("Offline ready")
+  }
+})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
